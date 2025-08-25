9cfd9b9389c21cb0fdae63f2b491a4691dc586d1
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-08-24 18:22:58 -0600

    Inicio Curso de Angular 2025 Con Fernando Herrera

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 'HOLA MI MUNDO EN TYPESCRIPT :)';
+
+console.log('Hola Mundo en TypeScript DESDE LA CONSOLA');
