05e579436f4cd4ed515c426aca6e15ac1ac5addd
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-08-26 21:31:39 -0600

    Clase 16. Completada. Tarea sobre objetos e interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 // import './01-topics/02-object-interfaces';
-import './01-topics/03-functions';
+// import './01-topics/03-functions';
+import './01-topics/04-homework-types';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
