c5645a51d63301805c413d6edb03a2bd91aa8b36
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-08-26 21:32:20 -0600

    Clase 16. Completada. Tarea sobre objetos e interfaces

---
+++ 01-typescript-intro/src/01-topics/04-homework-types.ts
@@ -0,0 +1,33 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface directions{
+    street: string;
+    country: string;
+    city: string;
+}
+
+interface SuperHero {
+    name: string;
+    age: number;
+    address: directions,
+    showAddress: () => string;
+}
+
+const superHeroe: SuperHero = {
+    name: 'Spiderman',
+    age: 30,
+    address: {
+        street: 'Main St',
+        country: 'USA',
+        city: 'NY',
+    },
+    showAddress() {
+        return this.name + ', ' + this.address.city + ', ' + this.address.country;
+    }
+}
+
+
+const address = superHeroe.showAddress();
+console.log( address );
