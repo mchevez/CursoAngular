e570e30992b7fcef56661a9272e8c11fef1eefdc
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-08-26 22:00:07 -0600

    Clase 17 Completada. Destructuracion de Objetos

---
+++ 01-typescript-intro/src/01-topics/05-basic-destructuring.ts
@@ -0,0 +1,38 @@
+
+interface AudioPlayer {
+
+    audioVolumen: number,
+    songDuration: number,
+    song: string
+    details: Details
+}
+
+interface Details {
+    author: string,
+    year: number
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolumen: 90,
+    songDuration: 36,
+    song: "Mess",
+    details: {
+        author: 'Ed Sheran',
+        year: 2015
+    }
+}
+
+const song = 'new song'
+
+const {
+    song:anotherSong, 
+    songDuration:Duration,
+    details 
+} = audioPlayer
+
+const { author } = details
+
+
+console.log('Song: ', anotherSong);
+console.log('Duration: ', Duration);
+console.log('Author: ', author);
--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 // import './01-topics/02-object-interfaces';
 // import './01-topics/03-functions';
-import './01-topics/04-homework-types';
+// import './01-topics/04-homework-types';
+import './01-topics/05-basic-destructuring';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
