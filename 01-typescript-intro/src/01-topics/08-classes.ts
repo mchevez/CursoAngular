7f8bd2adc5f0d47350dae5547f669d111dd26262
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-08-31 13:38:05 -0600

    Clase 25 Completada. Priorizar Composicion sobre herencia

--- 01-typescript-intro/src/01-topics/08-classes.ts
+++ 01-typescript-intro/src/01-topics/08-classes.ts
@@ -5,24 +5,41 @@ export class Person{
     // private address?: string;
 
     constructor(
-        public name: string,
+        public firstname: string,
+        public lastname: string,
         private address: string = 'No Address'
     ){ }
 }
 
-export class Hero extends Person{
+// export class Hero extends Person{
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+
+//     ){
+//         super(realName, 'New York');
+
+//     }
+// }
+
+export class Hero{
+    // public person: Person;
+
     constructor(
         public alterEgo: string,
         public age: number,
         public realName: string,
+        public person: Person
 
     ){
-        super(realName, 'New York');
-
+        // super(realName, 'New York');
+        // this.person = new Person(realName)
     }
 }
 
-const ironman = new Hero('IronMan', 45, 'Tony');
+const tony = new Person('Tony', 'Stark', 'New York');
+const ironman = new Hero('IronMan', 45, 'Tony', tony);
 
 console.log(ironman);
 // console.log(Michael.address);
