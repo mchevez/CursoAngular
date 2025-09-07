f055d7867c9ebb992df3a4c09b870276e68b95ae
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-07 16:03:34 -0600

    Clase 36 Completada. Counter Page - Componente

--- 02-Bases/src/app/app.routes.ts
+++ 02-Bases/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CounterPageComponent } from './counter/counter-pages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'',
+    component:CounterPageComponent
+  }
+];
---
+++ 02-Bases/src/app/counter/counter-pages.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  template: `
+    <h1>Counter {{counter}}</h1>
+    <button (click)="increaseBy(1)">+1</button>
+  `
+})
+export class CounterPageComponent{
+    counter = 15;
+
+    increaseBy(value: number){
+
+      this.counter += value;
+    }
+}
