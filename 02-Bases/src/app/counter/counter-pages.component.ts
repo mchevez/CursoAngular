2bd6df1cdb19dfe7328f10cb956d7ce2566658f7
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-07 17:50:06 -0600

    Clase 38 Completada. Señales

--- 02-Bases/src/app/counter/counter-pages.component.ts
+++ 02-Bases/src/app/counter/counter-pages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 
 @Component({
@@ -14,10 +14,15 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent{
     counter = 10;
+    counterSignal = signal(10);
 
     increaseBy(value: number){
-
       this.counter += value;
+      // this.counterSignal.set(this.counterSignal() + 1)
+      this.counterSignal.update(current => current + value);
     }
-    resetCounter = () => this.counter = 10;
+    resetCounter = () => {
+      this.counter = 10;
+      this.counterSignal.set(10);
+    };
 }
