8157d9af1c73cb0ec39114bce82278b81d9a7471
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-08 22:51:58 -0600

    Clase 41 Completada. Solucion Tarea

--- 02-Bases/src/app/app.routes.ts
+++ 02-Bases/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './counter/counter-pages.component';
+import { HeroPageComponent } from './hero/hero-page.component';
 
 export const routes: Routes = [
 
   {
     path:'',
     component:CounterPageComponent
-  }
+  },
+  {
+    path:'hero',
+    component:HeroPageComponent
+  },
+
 ];
