87e1bde620be197fe8fbc4237979e3697780a650
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-15 15:32:50 -0600

    Clase 54 Completada. Inputs - Agregar Personaje

--- 02-Bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ 02-Bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -13,12 +13,32 @@ interface Character{
   // imports: [NgClass],
 })
 export class DragonballPageComponent {
+
+  name = signal('Gohan');
+  power = signal(100);
+  Number = Number;
+
   characters = signal<Character[]>([
     {id: 1, name:'Goku', power: 9001 },
     {id: 2, name:'Vegeta', power: 8000 },
     {id: 4, name:'Yamcha', power: 500 },
     {id: 3, name:'Piccolo', power: 3000 },
   ])
+
+  addCharacter = () => {
+
+    let data = this.characters();
+
+    const character : Character = {
+      id: 0,
+      name: this.name(),
+      power: this.power(),
+    }
+
+    data.push(character);
+    this.name.set('');
+    this.power.set(0);
+  }
 
   // powerClasses = computed(() => {
 
