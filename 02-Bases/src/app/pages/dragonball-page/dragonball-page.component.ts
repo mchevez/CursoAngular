648cff7496afe5983cabc5b8b45a33e972a8207c
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-14 23:40:05 -0600

    Clase 51 Completada. @for - Iteraciones de elementos

--- 02-Bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ 02-Bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character{
+  id: number,
+  name: string,
+  power: number
+}
 
 @Component({
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
-
+  characters = signal<Character[]>([
+    {id: 1, name:'Goku', power: 9001 },
+    {id: 2, name:'Vegeta', power: 8000 },
+    {id: 3, name:'Piccolo', power: 3000 },
+  ])
 }
