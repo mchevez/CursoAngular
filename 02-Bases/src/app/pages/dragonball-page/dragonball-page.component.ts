c4e5424ae8a5dff9d1c7c45ed610171e17cbebae
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-15 14:27:21 -0600

    Clase 53 Completada. @if Mostrar elementos de forma condicional

--- 02-Bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ 02-Bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,4 @@
-import { NgClass } from '@angular/common';
+// import { NgClass } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
 
@@ -10,12 +10,13 @@ interface Character{
 
 @Component({
   templateUrl: './dragonball-page.component.html',
-  imports: [NgClass],
+  // imports: [NgClass],
 })
 export class DragonballPageComponent {
   characters = signal<Character[]>([
     {id: 1, name:'Goku', power: 9001 },
     {id: 2, name:'Vegeta', power: 8000 },
+    {id: 4, name:'Yamcha', power: 500 },
     {id: 3, name:'Piccolo', power: 3000 },
   ])
 
