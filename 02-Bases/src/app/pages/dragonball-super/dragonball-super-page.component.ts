6e038b8534608f2780259e2c53bec84d3925d620
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-16 13:15:50 -0600

    Clase 61 Completada. Efectos y LocalStorage

--- 02-Bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-Bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -16,12 +16,4 @@ export class DragonballSuperPageComponent {
 
   public dragonballServices = inject(DragonBallServices);
 
-  // characters = signal<Character[]>([
-  //   {id: 1, name:'Goku', power: 9001 },
-  //   {id: 2, name:'Vegeta', power: 8000 },
-  // ])
-
-  // addCharacter = (character: Character) => {
-  //   this.characters.update(list =>[...list, character ]);
-  // }
 }
--- 02-Bases/src/app/services/dragonball.service.ts
+++ 02-Bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interfaces';
 
 @Injectable({providedIn: 'root'})
@@ -8,6 +8,10 @@ export class DragonBallServices {
     {id: 1, name:'Goku', power: 9001 },
     {id: 2, name:'Vegeta', power: 8000 },
   ])
+
+  saveToLocalStorage = effect(() =>{
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
+  })
 
   addCharacter = (character: Character) => {
     this.characters.update(list =>[...list, character ]);
