ab45d01e80284fad2fe8cf215522b8a7e3ea5f92
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-18 15:44:23 -0600

    Clase 70 Completada. Rutas Hijas

---
+++ 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [RouterOutlet],
+  templateUrl: './search-page.component.html',
+})
+export default class SearchPageComponent { }
