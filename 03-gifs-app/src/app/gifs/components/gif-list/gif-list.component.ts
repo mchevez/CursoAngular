295ce8ec58a19eabeeb33691cbd808a49eb3d3e8
MICHAEL BRYANG CHEVEZ ANGULO <mchevez94@gmail.com>
2025-09-19 15:31:59 -0600

    Clase 75 Completada. Tarea - Inputs de Imagenes

--- 03-gifs-app/src/app/gifs/components/gif-list/gif-list.component.ts
+++ 03-gifs-app/src/app/gifs/components/gif-list/gif-list.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
+
 
 @Component({
   selector: 'gif-list',
   imports: [GifListItemComponent],
   templateUrl: './gif-list.component.html',
 })
-export class GifListComponent { }
+export class GifListComponent {
+
+  gifs = input.required<string[]>()
+ }
